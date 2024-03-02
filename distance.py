014f4ea41e93b6bd52fc7e2a3444028fdad829b7
Patryk <pathirny@googlemail.com>
2024-03-02 23:06:18 +0000

    3 led lights working

--- distance.py
+++ distance.py
@@ -1,11 +1,20 @@
-import gpiozero
+from gpiozero import LED
 import time
-led = gpiozero.LED(17)
 
+blue = LED(17)
+red = LED(27)
+yellow = LED(22)
 
 for x in range(10):
-    led.on()
-    time.sleep(1)
-    led.off()
-    time.sleep(1)
-    
+    blue.on()
+    time.sleep(0.1)
+    blue.off()
+    time.sleep(0.1)
+    red.on()
+    time.sleep(0.1)
+    red.off()
+    time.sleep(0.1)
+    yellow.on()
+    time.sleep(0.1)
+    yellow.off()
+    time.sleep(0.1)
