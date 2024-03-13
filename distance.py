9d4bfcb69917fa5063c04d528cca9139a13dfe62
Patryk <pathirny@googlemail.com>
2024-03-13 20:15:31 +0000

    turning LEds on

--- distance.py
+++ distance.py
@@ -3,14 +3,18 @@ from gpiozero import DistanceSensor
 import time
 
 # set the trigger and echo pin
-
+greenLed = 23
+yellowLed = 22
+redLed= 27
 PIN_TRIGGER = 4
 PIN_ECHO = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-
-
+# gpio set up
+GPIO.setup(redLed, GPIO.OUT)
+GPIO.setup(yellowLed, GPIO.OUT)
+GPIO.setup(greenLed, GPIO.OUT)
 GPIO.setup(PIN_TRIGGER, GPIO.OUT)
 GPIO.setup(PIN_ECHO, GPIO.IN)
 
@@ -30,6 +34,7 @@ def distance():
 
     startTime =  time.time()
     endTime = time.time()
+    
     while GPIO.input(PIN_ECHO) == 0:
         startTime = time.time()
     while GPIO.input(PIN_ECHO) == 1:
@@ -44,10 +49,19 @@ def distance():
 try:
     while True: 
         dist= distance()
-        print(f"Measured distance = {dist}cm")
-        time.sleep(1)
+        # print(f"Measured distance = {dist}cm")
+        # time.sleep(1)
+
+        if dist < 5:
+            GPIO.output(redLed, GPIO.HIGH)
+        elif dist > 5 and dist < 20:
+            GPIO.output(redLed, GPIO.LOW)
+            GPIO.output(yellowLed, GPIO.HIGH)
+        else:
+            GPIO.output(yellowLed, GPIO.LOW)
+            GPIO.output(greenLed, GPIO.HIGH)
 finally:
-    if KeyboardInterrupt == True:
+    if KeyboardInterrupt:
         print("Measurement Stopped by user")
         GPIO.cleanup()
 
