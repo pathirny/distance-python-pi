3cbf5f3abb667c0ba4d12f9efd0a155fe73896fa
Patryk <pathirny@googlemail.com>
2024-03-13 17:27:02 +0000

    Playing with LED

--- distance.py
+++ distance.py
@@ -1,5 +1,8 @@
 import RPi.GPIO as GPIO
-
+import time
 GPIO.setmode(GPIO.BCM)
-
-print("HELLO")
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(17, GPIO.LOW)
