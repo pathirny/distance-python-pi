63035b10b405b4d5a70282bbd274e84dd0ce8bac
Patryk Hirny <pathirny@googlemail.com>
2024-03-13 20:27:57 +0000

    attempting fix

--- distance.py
+++ distance.py
@@ -1,17 +1,18 @@
 import RPi.GPIO as GPIO
-from gpiozero import DistanceSensor
 import time
 
-# set the trigger and echo pin
-greenLed = 23
-yellowLed = 22
-redLed= 27
-PIN_TRIGGER = 4
-PIN_ECHO = 17
+# Set the GPIO mode and warnings
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# gpio set up
+# Define the GPIO pins for LEDs and ultrasonic sensor
+greenLed = 23
+yellowLed = 22
+redLed = 27
+PIN_TRIGGER = 4
+PIN_ECHO = 17
+
+# Set up GPIO pins
 GPIO.setup(redLed, GPIO.OUT)
 GPIO.setup(yellowLed, GPIO.OUT)
 GPIO.setup(greenLed, GPIO.OUT)
@@ -19,60 +20,42 @@ GPIO.setup(PIN_TRIGGER, GPIO.OUT)
 GPIO.setup(PIN_ECHO, GPIO.IN)
 
 def distance():
-# GPIO.setup(27, GPIO.OUT)
-# GPIO.output(27, GPIO.HIGH)
-# time.sleep(1)
-# GPIO.output(27, GPIO.LOW)
     GPIO.output(PIN_TRIGGER, True)
-
-    # print("Waiting for sensor...")
-    # time.sleep(2)
-
-    # print("Calculating distance...")
-
+    time.sleep(0.00001)
     GPIO.output(PIN_TRIGGER, False)
 
-    startTime =  time.time()
+    startTime = time.time()
     endTime = time.time()
-    
+
     while GPIO.input(PIN_ECHO) == 0:
         startTime = time.time()
     while GPIO.input(PIN_ECHO) == 1:
         endTime = time.time()
 
     duration = endTime - startTime
-
     distance = (duration * 34300) / 2
-
-    while distance:
-        if distance < 5:
-            GPIO.output(redLed, GPIO.HIGH)
-        elif distance > 5 and distance < 20:
-            GPIO.output(redLed, GPIO.LOW)
-            GPIO.output(yellowLed, GPIO.HIGH)
-        else:
-            GPIO.output(yellowLed, GPIO.LOW)
-            GPIO.output(greenLed, GPIO.HIGH)
 
     return distance
 
-
 try:
-     while True: 
-        dist= distance()
-        print(f"Measured distance = {dist}cm")
+    while True:
+        dist = distance()
+        print(f"Measured distance = {dist} cm")
         time.sleep(1)
 
         if dist < 5:
             GPIO.output(redLed, GPIO.HIGH)
-        elif dist > 5 and dist < 20:
+            GPIO.output(yellowLed, GPIO.LOW)
+            GPIO.output(greenLed, GPIO.LOW)
+        elif 5 <= dist < 20:
             GPIO.output(redLed, GPIO.LOW)
             GPIO.output(yellowLed, GPIO.HIGH)
+            GPIO.output(greenLed, GPIO.LOW)
         else:
+            GPIO.output(redLed, GPIO.LOW)
             GPIO.output(yellowLed, GPIO.LOW)
             GPIO.output(greenLed, GPIO.HIGH)
-finally:
-     if KeyboardInterrupt:
-         print("Measurement Stopped by user")
-         GPIO.cleanup()
 
+except KeyboardInterrupt:
+    print("Measurement Stopped by user")
+    GPIO.cleanup()
